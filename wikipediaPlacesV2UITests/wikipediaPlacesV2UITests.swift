80679cff9ebaeaf881df417d395d200ce695e96a
Rajeev Bhatia <rajeev.bhatia@MacBook-Pro.local>
2018-05-31 17:50:33 +0200

    added more UI tests

--- wikipediaPlacesV2UITests/wikipediaPlacesV2UITests.swift
+++ wikipediaPlacesV2UITests/wikipediaPlacesV2UITests.swift
@@ -27,4 +27,25 @@ class wikipediaPlacesV2UITests: XCTestCase {
         XCTAssert(app.tables.firstMatch.cells.count > 0, "no places visible for the user")
     }
     
+    //TODO: if the app is tested without pressing the "Open wikipedia" system alert, this test will fail. app.alerts is empty when the alert is shown
+    func testTableTap() {
+        
+        let cell = app.tables.firstMatch.cells.firstMatch
+        cell.tap()
+        
+//        if app.alerts.count > 0 { //the first time, the app might be showing the "open in wikipedia" alert
+//            app.alerts.firstMatch.buttons.firstMatch.tap()
+//        }
+        let waitForBg = app.wait(for: .runningBackground, timeout: 10.0)
+        XCTAssertTrue(waitForBg, "app Did Not go to background")
+    }
+    
+    func testMapOpen() {
+        
+        app.navigationBars.buttons.firstMatch.tap()
+        
+        XCTAssertTrue(app.maps.count > 0 , "map was not shown")
+        
+    }
+    
 }
